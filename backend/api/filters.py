65d66bad53699da30ec585c8f005cec9d940f35f
maximandreychuk1999@gmail.com <maximandreychuk1999@gmail.com>
2023-08-04 21:57:37 +0000

    after 1 review

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,6 @@
 from django_filters import rest_framework
-from recipes.models import Ingredient
+from django_filters.rest_framework import filters
+from recipes.models import Ingredient, Recipe, Tag
 from rest_framework.filters import SearchFilter
 
 class IngredientFilter(SearchFilter):
@@ -15,14 +16,20 @@ class RecipeFilter(rest_framework.FilterSet):
         field_name='author__id',
         lookup_expr='contains')
 
-    tags = rest_framework.CharFilter(
+    tags = rest_framework.ModelMultipleChoiceFilter(
         field_name='tags__slug',
-        lookup_expr='contains')
+        to_field_name='slug',
+        queryset=Tag.objects.all()
+    )
 
     is_favorited = rest_framework.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = rest_framework.BooleanFilter(
         method='get_is_in_shopping_cart'
     )
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart',)
 
     def get_is_favorited(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
