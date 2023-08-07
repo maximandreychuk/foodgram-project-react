fc1b8a13bd67b4d2bdc92dd08e8934f393f8ebc8
maximandreychuk1999@gmail.com <maximandreychuk1999@gmail.com>
2023-08-07 07:57:51 +0000

    after 1 review

--- backend/api/views.py
+++ backend/api/views.py
@@ -85,6 +85,7 @@ class TagViewSet(OnlyReadViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = None
 
 
 class Subscriptions(APIView, CustomPagination):
